649fc673d95680f681d898f139a1b5a715697189
rkhemka <rajatkhemka66@gmail,com>
2017-09-23 17:49:48 -0700

    Updated Home Work 1

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,38 +1,35 @@
-"""Files tests simple file read related operations"""
+
+from io import open
 
 class SimpleFile(object):
-    """SimpleFile tests using file read api to do some simple math"""
+
     def __init__(self, file_path):
         self.numbers = []
-        """
-        TODO: reads the file by path and parse content into two
-        dimension array (numbers)
-        """
+        f = open(file_path, encoding='utf-8')
+        text=f.read()
+        line = text.split('\n')
+        for i in line:
+            if(len (i)>0):
+                p= list(map (int, i.split(' ')))
+                self.numbers.append(p)
+
 
     def get_mean(self, line_number):
-        """
-        get_mean retrieves the mean value of the list by line_number (starts
-        with zero)
-        """
-        pass
+        line = self.numbers[line_number]
+        return sum(line)/float(len(line))   
+ 
 
     def get_max(self, line_number):
-        """
-        get_max retrieves the maximum value of the list by line_number (starts
-        with zero)
-        """
-        pass
+        line = self.numbers[line_number]
+        line.sort()
+        return line[-1]
 
     def get_min(self, line_number):
-        """
-        get_min retrieves the minimum value of the list by line_number (starts
-        with zero)
-        """
-        pass
+        line = self.numbers[line_number]
+        line.sort()
+        return line[0]
 
     def get_sum(self, line_number):
-        """
-        get_sum retrieves the sumation of the list by line_number (starts with
-        zero)
-        """
-        pass
+        line = self.numbers[line_number]
+        s1= sum(line)
+        return s1
